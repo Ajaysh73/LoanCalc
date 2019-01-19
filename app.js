9ca65ddb171310366e8ffc3926302e4846c95aae
ajay sharma <ajaysh73@gmail.com>
2019-01-19 14:09:38 -0500

    GUI upgrade & Final Release

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // Listen for submit
-document
-  .getElementById("loan-form")
-  .addEventListener("click", calculateResults);
+// document
+//   .getElementById("loan-form")
+//   .addEventListener("click", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function(e) {
+  document.getElementById("results").style.display = "none";
+  document.getElementById("loading").style.display = "block";
+  setTimeout(calculateResults, 2000);
+  e.preventDefault();
+});
 
 function calculateResults(e) {
   console.log("calculate clicked");
@@ -25,6 +31,47 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    // Show Results
+    document.getElementById("results").style.display = "block";
+
+    // Hide Loader
+    document.getElementById("loading").style.display = "none";
+  } else {
+    showError("Please check your numbers");
   }
-  e.preventDefault();
+//   e.preventDefault();
+}
+
+function showError(error) {
+  // Hide Results
+  document.getElementById("results").style.display = "none";
+
+  // Hide Loader
+  document.getElementById("loading").style.display = "none";
+
+  // Create Div
+  const errorDiv = document.createElement("div");
+
+  // Get elements
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+
+  // Add class
+  errorDiv.className = "alert alert-danger";
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 2000);
+}
+
+//Clear Error
+
+function clearError() {
+  document.querySelector(".alert").remove();
 }
