8589b21e8714416fbb28cd5adcb26993368535ae
ajay sharma <ajaysh73@gmail.com>
2019-01-19 13:38:57 -0500

    Loan Calculations applied

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+// Listen for submit
+document
+  .getElementById("loan-form")
+  .addEventListener("click", calculateResults);
 
+function calculateResults(e) {
+  console.log("calculate clicked");
+  // UI Vars
+  const amount = document.querySelector("#amount");
+  const interest = document.querySelector("#interest");
+  const years = document.querySelector("#years");
+  const monthlyPayment = document.querySelector("#monthly-payment");
+  const totalPayment = document.querySelector("#total-payment");
+  const totalInterest = document.querySelector("#total-interest");
+
+  const principal = parseFloat(amount.value);
+  const calculateInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculateInterest, calculatedPayments);
+  const monthly = (principal * x * calculateInterest) / (x - 1);
+  console.log(monthly);
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+  }
+  e.preventDefault();
+}
